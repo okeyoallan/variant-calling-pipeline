6c3863625b12fdc4a16e30506bf6355678dcb62e
allan <52036351+okeyoallan@users.noreply.github.com>
2022-01-25 12:12:06 +0300

    Update align.sh

--- templates/align.sh
+++ templates/align.sh
@@ -5,6 +5,6 @@
 bwa index ${ref_ch}
 
 #Aligning reads
-bwa mem -t 8 -R "@RG\\tID:${sample_id}\\tSM:${sample_id}\\tLB:${sample_id}\\tPU:${sample_id}\\tPL:ILLUMINA" -M ${ref_ch} ${Read1} ${Read2} > ${aligned}
+bwa mem -t 8 -R "@RG\\tID:${sample_id}\\tSM:${sample_id}\\tLB:${sample_id}\\tPU:${sample_id}\\tPL:ILLUMINA" -M ${ref_ch} ${read_R1} ${read_R2} > ${aligned}
 
 
