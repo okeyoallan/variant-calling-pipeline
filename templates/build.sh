0d28d4f36ee3840607f4f48b1f5c2df8e8eac537
allan <52036351+okeyoallan@users.noreply.github.com>
2022-02-02 12:11:17 +0300

    Update build.sh

--- templates/build.sh
+++ templates/build.sh
@@ -1,5 +1,6 @@
-#!/usr/bin/env bash
-wget https://snpeff.blob.core.windows.net/versions/snpEff_latest_core.zip
+#!/usr/bin/bash
+
+    wget https://snpeff.blob.core.windows.net/versions/snpEff_latest_core.zip
     unzip snpEff_latest_core.zip
     rm snpEff_latest_core.zip
         cd ./snpEff/
